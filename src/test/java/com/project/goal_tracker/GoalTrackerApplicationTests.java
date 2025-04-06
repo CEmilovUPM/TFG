63338c10807ce69ae59ed566bceb928562e00588
Cristian <c.emilov@alumnos.upm.es>
2025-04-06 20:51:18 +0200

    Initial commit

---
+++ src/test/java/com/project/goal_tracker/GoalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.goal_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
