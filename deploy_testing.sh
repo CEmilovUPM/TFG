c003db385ec33e1e786a7b19746f25769bb692e6
Cristian <c.emilov@alumnos.upm.es>
2025-06-10 09:05:29 +0200

    Minor fix for the deployment bash script

--- deploy_testing.sh
+++ deploy_testing.sh
@@ -1,10 +1,10 @@
 
 #this file has to be run in the linux machine
-kubectl delete -f backend/test/deploy.yaml --namespace=testing
+kubectl delete -f backend/deployment_files/test/deploy.yaml --namespace=testing
 kubectl delete -f database/test/deploy.yaml --namespace=testing
 
 git clone https://github.com/CEmilovUPM/TFG.git .;
 chmod +x mvnw;
 
 kubectl apply -f database/test/deploy.yaml --namespace=testing
-kubectl apply -f backend/test/deploy.yaml --namespace=testing
+kubectl apply -f backend/deployment_files/test/deploy.yaml --namespace=testing
