c3b1a68225aede72d4c771e590db3ee8cc4bb739
Cristian <c.emilov@alumnos.upm.es>
2025-06-10 09:48:19 +0200

    Disabled secrets.yaml

--- backend/src/main/java/com/project/goal_tracker/GoalTrackerApplication.java
+++ backend/src/main/java/com/project/goal_tracker/GoalTrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:secrets.properties")
+//@PropertySource("classpath:secrets.properties")
 public class GoalTrackerApplication {
 
 	public static void main(String[] args) {
