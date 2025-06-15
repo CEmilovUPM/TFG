e3f19f1e1ca39a07e1a790c3384c106220f06509
Cristian <c.emilov@alumnos.upm.es>
2025-06-15 16:47:41 +0200

    Deployment changes

--- deploy_testing.sh
+++ deploy_testing.sh
@@ -1,7 +1,9 @@
 
 #this file has to be run in the linux machine
+kubectl delete -f frontend/deployment_files/test/deploy.yaml --namespace=testing
 kubectl delete -f backend/deployment_files/test/deploy.yaml --namespace=testing
 kubectl delete -f database/test/deploy.yaml --namespace=testing
+
 
 git restore .;
 git pull;
@@ -11,3 +13,4 @@ chmod +x deploy_testing.sh
 
 kubectl apply -f database/test/deploy.yaml --namespace=testing
 kubectl apply -f backend/deployment_files/test/deploy.yaml --namespace=testing
+kubectl apply -f frontend/deployment_files/test/deploy.yaml --namespace=testing
