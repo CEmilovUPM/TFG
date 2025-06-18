572d102b7e204d2561abf9f3006cb25dd83df92a
Cristian <c.emilov@alumnos.upm.es>
2025-06-18 21:52:08 +0200

    Minor changes II

--- restart_testing.sh
+++ restart_testing.sh
@@ -3,6 +3,7 @@ git pull;
 echo cloned repository;
 chmod +x mvnw;
 chmod +x deploy_testing.sh
+chmod +x restart_testing.sh
 
 kubectl rollout restart deployment flask-frontend -n testing
 kubectl rollout restart deployment graph-microservice -n testing
