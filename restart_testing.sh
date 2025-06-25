f0aa053905028d7eb37af01249bed93327d5ad6d
Cristian <c.emilov@alumnos.upm.es>
2025-06-25 21:12:43 +0200

    More fixes

--- restart_testing.sh
+++ restart_testing.sh
@@ -2,7 +2,7 @@ git restore .;
 git pull;
 echo cloned repository;
 chmod +x mvnw;
-chmod +x deploy_testing.sh
+chmod +x deploy_testing.sh;
 chmod +x restart_testing.sh
 
 kubectl rollout restart deployment flask-frontend -n testing
